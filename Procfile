4b0145207b6b19b86e7bca893be55c61b45a7e84
jmccarey <joseph@mccarey.net>
2024-03-20 21:21:31 -0500

    remove default server from nginx

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/start-nginx -f gunicorn -c www/config.py wsgi:application --preload
+web: bin/start-nginx -f gunicorn -c www/config.py
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-pip install -r requirements.txt
-cp config/nginx.conf /etc/nginx
-nginx -s reload
-nginx -t
